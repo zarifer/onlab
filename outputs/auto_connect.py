1096c3cee15f4ee96675322ce9e06cc80f397c3a
Kitkat <55931207+zarifer@users.noreply.github.com>
2022-05-07 02:40:33 +0200

    Upload changes.

--- outputs/auto_connect.py
+++ outputs/auto_connect.py
@@ -6,14 +6,15 @@ import pandas as pd
 import os
 import subprocess
 
-files = os.listdir('/home/kitty/onlab/ml-sample-pack-small/benign/arm/')
+files = os.listdir('/home/nimda/Documents/ml-sample-pack-small/benign/arm/')
 malwy = '4eb6cb5191619d2d637beb4e6d4dde3aa58e7549b288f0d27916fe2ccd24be6a'
+donefiles = os.listdir('connect/')
 
 #Subprocess.PIPE = std.out messages go in a pipeline
 #Subprocess.Popen = opens a terminal, and calls the first argument, and separate them with space
 #process.communicate = Fetching (read and secure) the data to get out from the pipeline
-for file in files:
-        print(file)
-        process = subprocess.Popen(["python3", "connect.py", file, malwy], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        stdout, err = process.communicate()
-        print(stdout, err)
+for file in [f for f in files if all([f not in k for k in donefiles])]:
+	print(file)
+	process = subprocess.Popen(["python3", "connect.py", file, malwy], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	stdout, err = process.communicate()
+	print(stdout, err)
